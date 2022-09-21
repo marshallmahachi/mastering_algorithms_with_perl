a963ef1b36c44d68e0fa994d18f8a84605cb8029
marshall <marshall.mahachi@megaport.com>
2022-09-22 09:52:20 +1000

    reading the file in

--- chapter1/binary_search.pl
+++ chapter1/binary_search.pl
@@ -3,8 +3,21 @@
 use warnings;
 use strict;
 
-my ($filename) = @ARGV;
+print "usage: './binary_search <filename> <word>'\n" and exit if @ARGV != 2;
 
-print "the filename is :", $filename, "\n";
+my ($filename, $word) = @ARGV;
+
+print "filename :", $filename, "\n";
+print "word: ", $word, "\n";
+print "-" x 50, "\n";
+
+open(my $FH, "<", $filename)
+    or die "Can't open < $filename: $!";
+
+# we are reading the whole file to memory; which is not really ok
+my @word_arr = <$FH>;
+
+print "$filename has ", scalar @word_arr, " words.\n";
+
 
 exit;
